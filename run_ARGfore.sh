e1d40c8bba2174cd53f3728dc4fdf31a4a978770
miniymay <awdry@naver.com>
2024-08-06 16:06:27 -0400

    modify bash file

--- run_ARGfore.sh
+++ run_ARGfore.sh
@@ -4,3 +4,5 @@ dataFileName="./example/example_arg_dataset.csv"
 drugInfoFileName="./example/example_drug_info.csv"
 H="10" # the length of timepoints for the forecast period that the model will predict
 n="5" # Factor to be multipled to H (Determine the lengh of input n*H (e.g., 5*10))
+
+python3 argfore.py $dataFileName $drugInfoFileName $H $n
